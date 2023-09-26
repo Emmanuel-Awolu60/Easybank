2a369e5187d226e3ec5c0562418a4820eb6271d7
E O <114992756+Emmanuel-Awolu60@users.noreply.github.com>
2023-09-26 08:40:28 +0100

    update

--- starter/main.js
+++ starter/main.js
@@ -1,10 +1,15 @@
 let navMobile = document.querySelector(".mobile");
-let navclose = document.querySelector(".close");
+let navClose = document.querySelector(".close");
 let hamburger = document.querySelector(".hamburger");
 
 hamburger.addEventListener("click", function () {
   hamburger.classList.add("hidden");
-  navclose.classList.remove("hidden");
+  navClose.classList.remove("hidden");
   navMobile.classList.add("showMobile");
 });
 // function dosomething() {}
+navClose.addEventListener("click", function () {
+  hamburger.classList.remove("hidden");
+  navClose.classList.add("hidden");
+  navMobile.classList.rem("navMobile");
+});
