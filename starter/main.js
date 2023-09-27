d258a67ac3fa6882d559e890f063b6b471b62018
E O <114992756+Emmanuel-Awolu60@users.noreply.github.com>
2023-09-27 15:18:53 +0100

    add mediaquery and adedd some font

--- starter/main.js
+++ starter/main.js
@@ -1,15 +1,19 @@
 let navMobile = document.querySelector(".mobile");
 let navClose = document.querySelector(".close");
 let hamburger = document.querySelector(".hamburger");
+let firstSection = document.querySelector("first__section");
+let body = document.getElementsByName("body");
 
-hamburger.addEventListener("click", function () {
-  hamburger.classList.add("hidden");
-  navClose.classList.remove("hidden");
-  navMobile.classList.add("showMobile");
-});
-// function dosomething() {}
-navClose.addEventListener("click", function () {
-  hamburger.classList.remove("hidden");
-  navClose.classList.add("hidden");
-  navMobile.classList.rem("navMobile");
+function mobile() {
+  hamburger.classList.toggle("close");
+  navMobile.classList.toggle("showMobile");
+}
+
+body.addEventListener("click", function () {
+  navMobile.classList.add("hidden");
+  //   if (navMobile.classList !== "hidden") {
+  //     navMobile.classList.add("hidden");
+  //   } else {
+  //     navMobile.classList.remove("hidden");
+  //   }
 });
